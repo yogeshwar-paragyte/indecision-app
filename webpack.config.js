728d4ce3c2a026f9cf17c4deebbea69e0ce8814e
Yogeshwar Singh <ysthecool@gmail.com>
2018-07-15 15:34:45 +0530

    Webpack and ES6 Import Export. With Default export.

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+// entry -> output
+module.exports = {
+    entry: './src/app.js',
+    output:{
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+}
